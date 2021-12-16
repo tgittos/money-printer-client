eca953cb46be54019726890a84c11740334d82ae
Tim Gittos <hi@timgittos.com>
2021-12-16 14:17:32 -0600

    Pass in API url as env var

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,5 @@
 
 const apiHost = "localhost";
-//const apiHost = "api.moneyprintergobrr.io";
-//const apiHost = "development.eba-jgr836tj.us-west-2.elasticbeanstalk.com";
 
 const Env = {
   API_HOST: process.env.MP_API_HOST ?? apiHost,
